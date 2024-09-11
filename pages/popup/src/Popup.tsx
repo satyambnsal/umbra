ea7243be1eb5c972c643bed00f23dd8ab8488d7e
Satyam Bansal <satyamsgsits1994@gmail.com>
2024-09-11 18:25:30 +0530

    chrome setup

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -5,15 +5,38 @@ import { withErrorBoundary, withSuspense } from '@extension/shared';
 // import { exampleThemeStorage } from '@extension/storage';
 // import type { ComponentPropsWithoutRef } from 'react';
 import LoadingScreen from './components/top-loading';
-import { Router } from './router/router';
+// import { Router } from './router/router';
 import { ThemeProvider } from 'next-themes';
+import { useEffect, useState } from 'react';
+import type { PXE } from '@aztec/aztec.js';
+import { createPXEClient, waitForPXE } from '@aztec/aztec.js';
+import { RPC_URL } from './constants';
 
 const Popup = () => {
+  const [pxeClient, setPXEClient] = useState<PXE>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  useEffect(() => {
+    setErrorMessage('');
+    setIsLoading(true);
+    const pxe = createPXEClient(RPC_URL);
+    waitForPXE(pxe)
+      .then(_ => setPXEClient(pxe))
+      .catch(error => {
+        setErrorMessage(error.toString());
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+  console.log('pxe client', pxeClient);
   return (
     <>
       <LoadingScreen isLoading={false}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <Router />
+          {isLoading && <p>is loading</p>}
+          {errorMessage && <p>{errorMessage}</p>}
+          {/* <Router /> */}
         </ThemeProvider>
       </LoadingScreen>
     </>
--- pages/popup/vite.config.mts
+++ pages/popup/vite.config.mts
@@ -1,19 +1,29 @@
 import { resolve } from 'node:path';
 import { withPageConfig } from '@extension/vite-config';
 import svgr from 'vite-plugin-svgr';
-
+import viteResolve from 'vite-plugin-resolve';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 const rootDir = resolve(__dirname);
 const srcDir = resolve(rootDir, 'src');
 
+const aztecVersion = '0.51.0';
+
 export default withPageConfig({
-  plugins: [svgr()],
+  plugins: [nodePolyfills(), svgr()],
   resolve: {
     alias: {
       '@src': srcDir,
+      // '@aztec/bb.js': resolve(__dirname, './public/bb_0.51.0.js'),
     },
   },
   publicDir: resolve(rootDir, 'public'),
   build: {
     outDir: resolve(rootDir, '..', '..', 'dist', 'popup'),
+    target: 'esnext',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+    },
   },
 });
