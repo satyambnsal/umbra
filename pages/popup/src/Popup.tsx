62cd4da8cb6f0219454584e6fbedae3f84a4df22
Satyam Bansal <satyamsgsits1994@gmail.com>
2024-09-13 14:49:38 +0530

    Remove comments

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -1,62 +1,55 @@
 import '@src/Popup.css';
 import './assets/app.css';
 import { withErrorBoundary, withSuspense } from '@extension/shared';
-// import { exampleThemeStorage } from '@extension/storage';
-// import type { ComponentPropsWithoutRef } from 'react';
-// import LoadingScreen from './components/top-loading';
+import { exampleThemeStorage } from '@extension/storage';
+import LoadingScreen from './components/top-loading';
 import { ThemeProvider } from 'next-themes';
-// import { useCallback, useEffect, useState } from 'react';
-// import type { PXE } from '@aztec/aztec.js';
-// import { createPXEClient, waitForPXE } from '@aztec/aztec.js';
-// import { RPC_URL } from './constants';
-// import { RPC_URL } from './constants.js';
-import LoadingScreen from './components/top-loading.js';
+import { useCallback, useEffect, useState } from 'react';
+import type { PXE } from '@aztec/aztec.js';
+import { createPXEClient, waitForPXE } from '@aztec/aztec.js';
+import { RPC_URL } from './constants';
 import { Router } from './router/router.js';
-// import { createPXEClient, waitForPXE } from '@aztec/aztec.js';
-// import { useCallback, useEffect, useState } from 'react';
-// import { useAtom } from 'jotai';
-// import { walletStorage } from '@extension/storage';
-// import { pxeAtom } from './atoms.js';
-// import { useAtom } from 'jotai';
-// import { walletStorage } from '@extension/storage';
-// import { pxeAtom } from './atoms.js';
+import { walletStorage } from '@extension/storage';
+import { pxeAtom } from './atoms.js';
+import { useAtom } from 'jotai';
+import { useStorage } from '@extension/shared';
 
 const Popup = () => {
-  // const [pxeClient, setPXEClient] = useAtom(pxeAtom);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [errorMessage, setErrorMessage] = useState('');
-  // const walletData = useStorage(walletStorage);
-  // const handlePXEConnection = useCallback(() => {
-  //   setErrorMessage('');
-  //   setIsLoading(true);
-  //   const pxe = createPXEClient(walletData.rpcUrl);
-  //   waitForPXE(pxe)
-  //     .then(() => setPXEClient(pxe))
-  //     .catch(error => {
-  //       setErrorMessage(error.toString());
-  //     })
-  //     .finally(() => {
-  //       setIsLoading(false);
-  //     });
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [walletData.rpcUrl]);
+  const [pxeClient, setPXEClient] = useAtom(pxeAtom);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const walletData = useStorage(walletStorage);
+  const handlePXEConnection = useCallback(() => {
+    setErrorMessage('');
+    setIsLoading(true);
+    const pxe = createPXEClient(walletData.rpcUrl);
+    waitForPXE(pxe)
+      .then(() => setPXEClient(pxe))
+      .catch(error => {
+        setErrorMessage(error.toString());
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [walletData.rpcUrl]);
 
-  // useEffect(() => {
-  //   handlePXEConnection();
-  // }, [handlePXEConnection]);
+  useEffect(() => {
+    handlePXEConnection();
+  }, [handlePXEConnection]);
 
-  // console.log('pxe client 44', pxeClient);
+  console.log('pxe client 44', pxeClient);
   return (
     <>
       <LoadingScreen isLoading={false}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {/* {isLoading && <p>is loading</p>}
+          {isLoading && <p>is loading</p>}
           {errorMessage && (
             <div>
               <p>{errorMessage}</p>
               <button onClick={handlePXEConnection}>Try Reconnect</button>
             </div>
-          )} */}
+          )}
           <Router />
         </ThemeProvider>
       </LoadingScreen>
