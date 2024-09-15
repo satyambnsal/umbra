dcd7b2ffc75d3d13be6787f0e16fe2d7e2ee3c4f
Satyam Bansal <satyamsgsits1994@gmail.com>
2024-09-15 22:59:23 +0530

    Provider integration changes

--- pages/content/src/index.ts
+++ pages/content/src/index.ts
@@ -3,3 +3,55 @@ import { toggleTheme } from '@src/toggleTheme';
 console.log('content script loaded');
 
 void toggleTheme();
+
+// document.addEventListener('click', event => {
+//   if(event && event.target.matches('#connet-wallet')) {
+
+//   }
+// })
+
+const site = window.location.hostname;
+
+import { deserializeError } from 'serialize-error';
+import { onMessage, sendMessage } from 'webext-bridge/content-script';
+import { runtime } from 'webextension-polyfill';
+
+onMessage('umbra_event', async ({ data }) => {
+  const umbraEvent = new CustomEvent('umbra_event', {
+    detail: data,
+  });
+  window.dispatchEvent(umbraEvent);
+});
+
+const inject = () => {
+  if (typeof document === 'undefined') return;
+  const script = document.createElement('script');
+  script.src = runtime.getURL('/rpc.js');
+  script.type = 'module';
+  document.documentElement.appendChild(script);
+  const channel = new BroadcastChannel('umbra');
+  channel.addEventListener('message', async ({ data }) => {
+    const origin = window.location.origin;
+    const responseChannel = new BroadcastChannel(data.respondAt);
+    if (!data.isUmbra) return responseChannel.postMessage({ error: 'Wrong context' });
+    let response;
+    try {
+      const result: any = await sendMessage(data.method, { ...data.payload, origin }, 'background');
+      if (result?.error) {
+        response = { jsonrpc: '1.0', error: deserializeError(result.error) };
+      } else {
+        response = { jsonrpc: '1.0', result };
+      }
+      return responseChannel.postMessage({
+        response,
+      });
+    } catch (error) {
+      response = { jsonrpc: '1.0', error };
+      return responseChannel.postMessage({
+        response,
+      });
+    }
+  });
+};
+
+inject();
